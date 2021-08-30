30473de67d6d2266ea60c1669a6fe50b022225bb
sfdevelop <sfdevelop747@gmail.com>
2021-08-31 01:53:13 +0300

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index(){
+        return view('layouts.admin_layouts');
+    }
+}
---
+++ resources/views/layouts/admin/head.blade.php
@@ -0,0 +1,24 @@
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="author" content="Sfdevelop">
+
+    <title>Admin Panel</title>
+    <link rel="apple-touch-icon" href="{{asset('admin/assets/images/favicon/apple-touch-icon-152x152.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('admin/assets/images/favicon/favicon-32x32.png')}}">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/vendors/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/vendors/animate-css/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/pages/charts-chartist.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/vendors/chartist-js/chartist-plugin-tooltip.css')}}">
+
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/themes/vertical-modern-menu-template/materialize.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/themes/vertical-modern-menu-template/style.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/pages/dashboard-modern.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('admin/assets/css/pages/intro.min.css')}}">
+
+    <link rel="stylesheet" type="text/css" href=".{{asset('admin/assets/css/custom/custom.css')}}">
+</head>
